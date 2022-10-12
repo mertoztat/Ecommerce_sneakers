e7dfee0f732441f273a03263e3212226511f0de7
Mert Öztat <mertoztat@gmail.com>
2022-10-12 15:45:58 +0300

    Cart Item Css Fixed

--- src/components/CartItem/index.jsx
+++ src/components/CartItem/index.jsx
@@ -7,17 +7,22 @@ const CartItem = ({ item }) => {
   const { removeCart } = useContext(CartContext);
 
   return (
-    <li>
-      <img src={item.img} alt="" />
-      <div className="cart_desc">
-        <h4 className="cart_title">{item.title}</h4>
-        <p className="cartSpan">
-          {` ${item.price} * ${item.count} `}
-          <span>${item.price * item.count}</span>
-          <img src={trash} alt="" onClick={() => removeCart(item.id)} />
-        </p>
+    <>
+      <li>
+        <img src={item.img} alt="" />
+        <div className="cart_desc">
+          <h4 className="cart_title">{item.title}</h4>
+          <p className="cartSpan">
+            {` ${item.price} * ${item.count} `}
+            <span>${item.price * item.count}</span>
+            <img src={trash} alt="" onClick={() => removeCart(item.id)} />
+          </p>
+        </div>
+      </li>
+      <div className="cart_btn">
+        <button className="btn_checkout">Checkout</button>
       </div>
-    </li>
+    </>
   );
 };
 
